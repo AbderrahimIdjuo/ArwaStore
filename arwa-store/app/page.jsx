823eb677ef82d560958697f84d0ccd03a7cc616e
Abdo Idjuo <abderrahimoujjdi@gmail.com>
2024-10-25 21:41:53 +0100

    version 2.1.1

--- arwa-store/app/page.jsx
+++ arwa-store/app/page.jsx
@@ -1,13 +1,10 @@
 import "./globals.css";
 import { NavbarWithSolidBackground as NavBar } from "./components/NavBar1";
-import LoadingCommandesTable from "./components/LoadingCommandesTable";
 
 export default async function Home() {
   return (
     <>
       <NavBar />
-      <div className="container">
-      </div>
     </>
   );
 }
